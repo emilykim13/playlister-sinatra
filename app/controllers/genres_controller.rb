0c94ee65ad5dbbd26a900b5aca7ee03cc846d02f
github username <github email address>
2021-03-18 16:54:40 -0500

    :) not screaming

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -7,5 +7,10 @@ class GenresController < ApplicationController
         @genres = Genre.all 
         erb :index
     end
+
+    get "/genres/:slug" do 
+        @genre = Genre.find_by_slug(params[:slug])
+        erb :show
+    end
     
 end
