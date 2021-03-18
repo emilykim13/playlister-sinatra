7f903906608af65ba44a4cbb564ee94e8d4a353a
Cori Blount <heycorihey@icloud.com>
2021-03-18 15:27:22 -0500

    screamingggg

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+require 'rack-flash'
 class SongsController < ApplicationController
     set :views, "app/views/songs"
 
@@ -5,5 +6,24 @@ class SongsController < ApplicationController
         @songs = Song.all 
         erb :index
     end
-    
+
+    get '/songs/new' do
+        erb :new
+    end
+
+    get '/songs/:slug' do
+        @songs = Song.find_by_slug(params[:slug])
+        erb :show
+    end
+
+    post '/songs' do
+        @songs = Song.create(params[:song])
+        @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+        @song.genres_id = params[:genres]
+        @song.save
+        flash[:message] = "Successfully created song."
+        redirect ("/songs/#{@song.slug}")
+      end
+
+
 end
